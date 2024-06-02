962c0d6df180172f3749d85d674e5d5b2a9913ad
KeithG33 <keith.gordon9@gmail.com>
2024-06-01 18:34:30 -0700

    change to pure python to avoid multiprocessing errors, convert to tensor in getitem

--- data/dataset.py
+++ data/dataset.py
@@ -18,60 +18,126 @@ PLAYER_TYPE = 3
 NANA_TYPE = 4
 ACTION_TYPE = 5
 
+# def generate_input_tensor(observation, prev_action, player_index):
+#     """
+#     Generate the input tensor from the observation. Create a sequence of
+#     features to handle the variable sized number of players and projectiles
+
+#     First index indicates whether tensor corresponds to player, projectile, nana,
+#     or misc info. Negative value indicates currently active player.
+
+#     Also includes prev action in observation 
+#     """
+    
+#     all_tensors = []
+
+#     # misc_info includes distance, frame, and stage
+#     # players is a list of lists of length 20
+#     # nanas is a list of lists of length 20
+#     # projectiles is a list of lists of length 8
+#     misc = torch.tensor(observation[:3]).view(1, 3)
+#     players = observation[4:]  # 2 to 4 players
+#     projectiles = observation[3]
+#     nana_states = [obs.pop(9) for obs in players]
+    
+#     misc_types = torch.full((1, 1), MISC_TYPE)
+#     misc_padded = F.pad(misc, (0, 20 - misc.shape[1]))
+#     misc = torch.cat([misc_types, misc_padded], dim=1)
+#     all_tensors.append(misc)
+    
+#     players = torch.tensor(players)
+#     player_types = torch.full((players.shape[0], 1), PLAYER_TYPE)
+#     player_types[player_index] = -PLAYER_TYPE  # Neg value for active player
+#     players = torch.cat([player_types, players], dim=1)
+#     all_tensors.append(players)
+
+#     nana_list = []
+#     for i, nana in enumerate(nana_states):
+#         if nana is not None:
+#             nana_tensor = torch.tensor(nana).view(1, -1)
+#             nana_type = -NANA_TYPE if i == player_index else NANA_TYPE
+#             nana_types = torch.full((1, 1), nana_type)
+#             nana_tensor = torch.cat([nana_types, nana_tensor], dim=1)
+#             nana_list.append(nana_tensor)
+#     if nana_list: 
+#         nana_tensors = torch.cat(nana_list, dim=0)
+#         all_tensors.append(nana_tensors)
+
+#     # TODO: experiment conditioning on previous action
+#     #
+#     # if prev_action is not None:
+#     #     prev_action = prev_action + [0] * (20 - len(prev_action))
+#     #     prev_action = torch.tensor(prev_action).view(1, -1)
+#     # else:
+#     #     prev_action = torch.zeros(1, 20)
+#     # action_types = torch.full((len(prev_action), 1), ACTION_TYPE)
+#     # action_tensors = torch.cat([action_types, prev_action], dim=1)
+#     # all_tensors.append(action_tensors)
+
+#     if projectiles:
+#         projectile_tensors = torch.tensor(projectiles)
+#         projectile_types = torch.full((projectile_tensors.shape[0], 1), PROJECTILE_TYPE)
+#         projectile_padded = F.pad(projectile_tensors, (0, 20 - projectile_tensors.shape[1]))
+#         projectiles = torch.cat([projectile_types, projectile_padded], dim=1)
+#         all_tensors.append(projectiles)
+        
+#     combined_tensor = torch.cat(all_tensors, dim=0)  # (seq, 21)
+#     return combined_tensor
+
+
 # TODO: the only difference here is the player_index parts
 #       Only need to call this once and then manipulate the player indices
-def generate_input_combined(observation, prev_action, player_index):
+def generate_input_python(observation, prev_action, player_index):
     """
-    Generate the input tensor from the observation. Create a sequence of
-    features to handle the variable sized number of players and projectiles
+    Generate the input data from the observation using plain Python lists,
+    converting the data structure to only use lists and preparing it for later
+    conversion to tensors if needed outside of this function.
 
-    First index indicates whether tensor corresponds to player, projectile, nana,
+    First index indicates whether the data corresponds to player, projectile, nana,
     or misc info. Negative value indicates currently active player.
-
-    Also includes prev action in observation 
     """
 
-    MISC_TYPE = 1
-    PROJECTILE_TYPE = 2
-    PLAYER_TYPE = 3
-    NANA_TYPE = 4
-    ACTION_TYPE = 5
-    
     all_tensors = []
 
     # misc_info includes distance, frame, and stage
-    # players is a list of lists of length 20
-    # nanas is a list of lists of length 20
-    # projectiles is a list of lists of length 8
-    misc = torch.tensor(observation[:3]).view(1, 3)
-    players = observation[4:]  # 2 to 4 players
+    misc = observation[:3]  # Assuming this slices correctly
+    players = observation[4:]  # Assuming players data start from index 4
     projectiles = observation[3]
-    nana_states = [obs.pop(9) for obs in players]
-    
-    misc_types = torch.full((1, 1), MISC_TYPE)
-    misc_padded = F.pad(misc, (0, 20 - misc.shape[1]))
-    misc = torch.cat([misc_types, misc_padded], dim=1)
+    nana_states = [obs.pop(9) for obs in players]  # Modify this if structure differs
+
+    # Creating misc tensor data
+    misc_types = [MISC_TYPE]
+    misc_padded = misc + [0] * (20 - len(misc))
+    misc = misc_types + misc_padded
     all_tensors.append(misc)
     
-    players = torch.tensor(players)
-    player_types = torch.full((players.shape[0], 1), PLAYER_TYPE)
-    player_types[player_index] = -PLAYER_TYPE  # Neg value for active player
-    players = torch.cat([player_types, players], dim=1)
-    all_tensors.append(players)
+    # Processing players
+    players_list = []
+    for i, player in enumerate(players):
+        player_type = [-PLAYER_TYPE] if i == player_index else [PLAYER_TYPE]
+        player_data = player_type + player + [0] * (20 - len(player))
+        players_list.append(player_data)
+    all_tensors.extend(players_list)
 
+    # Processing Nana states
     nana_list = []
     for i, nana in enumerate(nana_states):
         if nana is not None:
-            nana_tensor = torch.tensor(nana).view(1, -1)
-            nana_type = -NANA_TYPE if i == player_index else NANA_TYPE
-            nana_types = torch.full((1, 1), nana_type)
-            nana_tensor = torch.cat([nana_types, nana_tensor], dim=1)
-            nana_list.append(nana_tensor)
-    if nana_list: 
-        nana_tensors = torch.cat(nana_list, dim=0)
-        all_tensors.append(nana_tensors)
+            nana_type = [-NANA_TYPE] if i == player_index else [NANA_TYPE]
+            nana_data = nana_type + nana + [0] * (20 - len(nana))
+            nana_list.append(nana_data)
+    all_tensors.extend(nana_list)
 
-    # TODO: experiment conditioning on previous action
+    # Handling projectiles
+    if projectiles:
+        projectile_list = []
+        for projectile in projectiles:
+            projectile_type = [PROJECTILE_TYPE]
+            projectile_data = projectile_type + projectile + [0] * (20 - len(projectile))
+            projectile_list.append(projectile_data)
+        all_tensors.extend(projectile_list)
+    
+    # TODO: experiment conditioning on previous action. Switch this to python lists
     #
     # if prev_action is not None:
     #     prev_action = prev_action + [0] * (20 - len(prev_action))
@@ -82,37 +148,27 @@ def generate_input_combined(observation, prev_action, player_index):
     # action_tensors = torch.cat([action_types, prev_action], dim=1)
     # all_tensors.append(action_tensors)
 
-    if projectiles:
-        projectile_tensors = torch.tensor(projectiles)
-        projectile_types = torch.full((projectile_tensors.shape[0], 1), PROJECTILE_TYPE)
-        projectile_padded = F.pad(projectile_tensors, (0, 20 - projectile_tensors.shape[1]))
-        projectiles = torch.cat([projectile_types, projectile_padded], dim=1)
-        all_tensors.append(projectiles)
-        
-    combined_tensor = torch.cat(all_tensors, dim=0)  # (seq, 21)
-    return combined_tensor
+    return all_tensors
 
 
 class BucketBatchSampler(Sampler):
     def __init__(self, sequences, batch_size):
         self.batch_size = batch_size
-        self.index_and_lengths = [(index, sequence.shape[0]) for index, sequence in enumerate(sequences)]
+        self.index_and_lengths = [(index, len(sequence)) for index, sequence in enumerate(sequences)]
         self.batches = self._create_batches()
         self.total_batches = len(self.batches)
 
     def _create_batches(self):
-        # Shuffle index and lengths to randomize bucket grouping
+        ''' Create mapping of indices to sequence length so we can batch in our dataset '''
+
         shuffle(self.index_and_lengths)
         
-        # Organize sequences by their length for bucketing
         length_to_indices_map = defaultdict(list)
         for index, length in self.index_and_lengths:
             length_to_indices_map[length].append(index)
 
-        # Create batches from the length-sorted index list
         batched_indices = []
         for indices in length_to_indices_map.values():
-            # Creating batches within each bucket
             for batch_start in range(0, len(indices), self.batch_size):
                 batched_indices.append(indices[batch_start:batch_start + self.batch_size])
         return batched_indices
@@ -121,7 +177,6 @@ class BucketBatchSampler(Sampler):
         return len(self.index_and_lengths)
 
     def __iter__(self):
-        # Shuffle batches to ensure model does not learn any order
         shuffle(self.batches)
         for batch in self.batches:
             yield batch
@@ -161,8 +216,8 @@ class SmashBrosDataset(Dataset):
             actions = d["actions"]
 
             for i in range(len(actions)):
-                player_input = generate_input_combined(copy.deepcopy(obs), None, i)
-                player_output = torch.tensor(actions[i])
+                player_input = generate_input_python(copy.deepcopy(obs), None, i)
+                player_output = actions[i]
                 inputs.append(player_input)
                 outputs.append(player_output)
 
@@ -172,8 +227,8 @@ class SmashBrosDataset(Dataset):
         return len(self.outputs)
 
     def __getitem__(self, index):
-        return self.inputs[index], self.outputs[index]
-
+        return torch.tensor(self.inputs[index]), torch.tensor(self.outputs[index])
+    
 
 if __name__ == "__main__":
     # Test the dataset
@@ -188,7 +243,7 @@ if __name__ == "__main__":
     testing_data = test_data + train_data
 
     t1 = time.perf_counter()
-    dataset = SmashBrosDataset(testing_data[:4], num_processes=1)
+    dataset = SmashBrosDataset(testing_data[:4], num_processes=4)
     print(f"Loaded {len(dataset)} data points in {time.perf_counter() - t1:.2f} seconds")
     # for pkl_file in testing_data:
     #     t1 = time.perf_counter()
