e70c97314d014bdd203911a7cf0a2688e7b1870f
KeithG33 <keith.gordon9@gmail.com>
2024-06-16 19:36:54 -0700

    train fix and adjustments

--- train.py
+++ train.py
@@ -8,39 +8,48 @@ import torch.nn.functional as F
 
 import wandb 
 
-from data.dataset import SmashBrosDataset, BucketBatchSampler, MISC_TYPE, ACTION_TYPE, PROJECTILE_TYPE, PLAYER_TYPE, NANA_TYPE
-from model.smash_transformer import SmashTransformer
+from smashbot.data.dataset import SmashBrosDataset, SequenceBatchSampler, MISC_TYPE, ACTION_TYPE, PROJECTILE_TYPE, PLAYER_TYPE, NANA_TYPE
+from smashbot.model.smash_transformer import SmashTransformer
 
-PKL_DIR_TRAIN = '/home/kage/smashbot_workspace/dataset/Slippi_Public_Dataset_v3/pickle/train'
-PKL_DIR_TEST = '/home/kage/smashbot_workspace/dataset/Slippi_Public_Dataset_v3/pickle/test'
+DATA_DIR_TRAIN = '/home/kage/smashbot_workspace/dataset/Slippi_Public_Dataset_v3/hickle_shuffle/train'
+DATA_DIR_TEST = '/home/kage/smashbot_workspace/dataset/Slippi_Public_Dataset_v3/hickle_shuffle/test'
 
-DATASET_SIZE_TRAIN = 30
-DATASET_SIZE_TEST = 1
-DATASET_PROCESSES = 30
+DATASET_SIZE_TRAIN = 25
+DATASET_SIZE_TEST = 4
+DATASET_PROCESSES = 1
 
-BATCH_SIZE_TRAIN = 1024
-BATCH_SIZE_TEST = 1024
+BATCH_SIZE_TRAIN = 1500
+BATCH_SIZE_TEST = 1500
 
-VALIDATION_EVERY = 5000
-LOG_EVERY = 100
+VALIDATION_EVERY = 20_000
+LOG_EVERY = 250
 NUM_ROUNDS = 100
-EPOCHS_PER_ROUND = 2
+EPOCHS_PER_ROUND = 1
 
 MODEL_SAVEPATH = 'SmashBotTransformer.pt'
 
+
+def sample_data(directory, num_samples):
+    input_files = [f.path for f in os.scandir(directory) if "inputs" in f.name]
+    sampled_input_files = random.sample(input_files, num_samples)
+    sampled_output_files = [f.replace("inputs", "outputs") for f in sampled_input_files]
+    return list(zip(sampled_input_files, sampled_output_files))
+
 def run_validation(model):
-    test_data = [pkl.path for pkl in os.scandir(PKL_DIR_TEST) if pkl.name.endswith(".pkl")]
-    sampled_test_data = random.sample(test_data, DATASET_SIZE_TEST)
+    sampled_test_data = sample_data(DATA_DIR_TEST, DATASET_SIZE_TEST)
     num_processes = min(DATASET_PROCESSES, DATASET_SIZE_TEST)
+    t1 = time.perf_counter()
     test_dataset = SmashBrosDataset(sampled_test_data, num_processes=num_processes)
-    sampler = BucketBatchSampler(test_dataset.inputs, BATCH_SIZE_TEST)
+    sampler = SequenceBatchSampler(test_dataset, BATCH_SIZE_TEST)
     val_loader = DataLoader(test_dataset, batch_sampler=sampler)   
-    print(f"Successfully loaded validation dataset with {len(test_dataset)} positions")
+    print(f"Loaded validation dataset with {len(test_dataset)} positions - {time.perf_counter()-t1} seconds")
 
     model.eval()
 
     t1 = time.perf_counter()
     total_loss = 0.0
+    total_buttons_loss = 0.0
+    total_sticks_loss = 0.0
     total_count = 0
 
     with torch.no_grad():
@@ -51,11 +60,17 @@ def run_validation(model):
             pred_policy = model(input)
                         
             loss, buttons_loss, sticks_loss = model.calculate_loss(pred_policy, target)
-            total_loss += loss.item() * input.size(0)  # Multiply loss by batch size to get total loss for this batch
-            total_count += input.size(0)  # Accumulate the total number of examples processed
+            total_loss += loss.item() * input.size(0)  # Multiply by batch to get total
+            total_buttons_loss += buttons_loss.item() * input.size(0)
+            total_sticks_loss += sticks_loss.item() * input.size(0)
+            total_count += input.size(0) 
+            # to lower power-consumption/heat
+            time.sleep(0.1)
 
     avg_loss = total_loss / total_count  # Compute average loss  
-    return avg_loss, buttons_loss.item(), sticks_loss.item()
+    avg_buttons_loss = total_buttons_loss / total_count
+    avg_sticks_loss = total_sticks_loss / total_count
+    return avg_loss, avg_buttons_loss, avg_sticks_loss
 
 
 def training_round(model, train_loader, num_epochs=10, log_every=1000, validation_every=20_000):
@@ -64,7 +79,7 @@ def training_round(model, train_loader, num_epochs=10, log_every=1000, validatio
     # Pytorch train stuffs
     optimizer = torch.optim.AdamW(model.parameters(), lr=9e-5)
     # optimizer = torch.optim.SGD(model.parameters(), lr=1e-3) 
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=50_000)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=100_000)
     grad_scaler = torch.cuda.amp.GradScaler()
 
     for epoch in range(num_epochs): 
@@ -93,7 +108,7 @@ def training_round(model, train_loader, num_epochs=10, log_every=1000, validatio
             if not skip_lr_sched: scheduler.step()
 
             if i % log_every == 0:
-                print(f"Epoch {epoch}, Iteration {i}, Loss: {loss}, Buttons Loss: {buttons_loss}, Sticks Loss: {sticks_loss}")
+                # print(f"Epoch {epoch}, Iteration {i}, Loss: {loss}, Buttons Loss: {buttons_loss}, Sticks Loss: {sticks_loss}")
                 wandb.log({
                     "lr": scheduler.get_last_lr()[0],
                     "train_loss": loss.item(),
@@ -105,7 +120,7 @@ def training_round(model, train_loader, num_epochs=10, log_every=1000, validatio
             if i % validation_every == 0 and i > 0 :
                 val_loss, val_buttons_loss, val_sticks_loss = run_validation(model)
                 print(f"Validation loss: {val_loss}")
-                
+            
                 wandb.log({
                     "val_loss": val_loss,
                     "val_buttons_loss": val_buttons_loss,
@@ -115,23 +130,25 @@ def training_round(model, train_loader, num_epochs=10, log_every=1000, validatio
                 if val_loss < best_val_loss:
                     best_val_loss = val_loss
                     torch.save(model.state_dict(), MODEL_SAVEPATH)
+                
+                model.train()
+            
+            # to lower power-consumption/heat
+            time.sleep(0.1)
 
         print(f"Epoch took {time.perf_counter()-t1} seconds ")
         torch.save(model.state_dict(), MODEL_SAVEPATH)
 
 
 def run_training(num_rounds, model):
-    train_data = [pkl.path for pkl in os.scandir(PKL_DIR_TRAIN) if pkl.name.endswith(".pkl")]
-
-    wandb.init(project="smashbot", id='caswh4xq', resume='must')
     for round in range(num_rounds):
         print(f"Starting round {round}")
         # build dataset 
-        # randomly sample dataset_size pgn files 
+        # randomly sample dataset_size files 
         t1 = time.perf_counter()
-        sampled_train_data = random.sample(train_data, DATASET_SIZE_TRAIN)
+        sampled_train_data = sample_data(DATA_DIR_TRAIN, DATASET_SIZE_TRAIN)
         train_dataset = SmashBrosDataset(sampled_train_data, num_processes=DATASET_PROCESSES)
-        sampler = BucketBatchSampler(train_dataset.inputs, BATCH_SIZE_TRAIN)
+        sampler = SequenceBatchSampler(train_dataset, BATCH_SIZE_TRAIN)
         train_loader = DataLoader(train_dataset, batch_sampler=sampler)
         print(f"Successfully loaded dataset with {len(train_dataset)} images - {time.perf_counter()-t1} seconds")
     
@@ -139,13 +156,13 @@ def run_training(num_rounds, model):
 
 
 if __name__ == "__main__":
-    model = SmashTransformer(action_dim=10, embed_dim=224, model_dim=480, nhead=24, num_layers=6, dropout=0.05)
+    model = SmashTransformer(action_dim=10, embed_dim=224, model_dim=768, nhead=24, num_layers=16, dropout=0.1)
     model = model.cuda()
     print(f"Model parameters: {sum(p.numel() for p in model.parameters())}")
-    MODEL_SAVEPATH = '/home/kage/smashbot_workspace/SmashBotTransformer.pt'
-    # load weights from SAVEPATH if it exists
-    if os.path.exists(MODEL_SAVEPATH):
-        model.load_state_dict(torch.load(MODEL_SAVEPATH))
+    
+    if os.path.exists('/home/kage/smashbot_workspace/SmashBotTransformer.pt'):
+        model.load_state_dict(torch.load('/home/kage/smashbot_workspace/SmashBotTransformer.pt'))
         print("Loaded model weights from previous training session")
 
+    wandb.init(project="smashbot", id='9jbgdf7f', resume='must')
     run_training(NUM_ROUNDS, model) 
